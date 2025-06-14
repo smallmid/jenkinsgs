71da457078a21ee431e2e01b1de270421533ab5e
smallmid <msmall@inoserv.com>
2025-06-14 14:31:06 -0400

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -31,7 +31,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.classs, args);
+		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
 }
